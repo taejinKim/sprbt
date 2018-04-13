31559b10bbb8e1a9b4684182eef01629a274aeef
blackdove_home <blackdove@naver.com>
2018-04-13 09:01:24 +0900

    프로젝트 생성0000

---
+++ src/main/java/kr/co/ndev/Application.java
@@ -0,0 +1,13 @@
+//ÂüÁ¶ : http://jojoldu.tistory.com/250
+package kr.co.ndev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
